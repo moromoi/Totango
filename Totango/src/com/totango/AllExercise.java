65358b748f9ae15ff437ad83c0d176f9424544e5
moromoi <alex.palchisky@gmail.com>
2018-11-28 18:58:42 +0200

    Add files via upload

---
+++ Totango/src/com/totango/AllExercise.java
@@ -0,0 +1,65 @@
+package com.totango;
+
+import java.util.Arrays;
+
+public class AllExercise {
+
+	public void exercise_01 () {
+
+		System.out.println("Exercise 01\n");
+
+		//Temp values
+		int max = 0;
+		int secondMax = 0;
+
+		//My array
+		int [] num = {13, 35, 2, 11, 33, 2, 34};
+
+		//Print all array 
+		System.out.println("Numbers in array: " + Arrays.toString(num));
+
+		//Find biggest number in array
+		for (int i = 0; i < num.length; i++) {
+			if(num[i] > max) {
+				max = num[i];
+			}
+		}
+		//Print biggest number in array
+		System.out.println("Biggest number in array: " + max);
+
+		//Find second biggest number in array
+		for (int i = 0; i < num.length; i++) {
+			if((num[i] > secondMax) && (num[i] != max)) {
+				secondMax = num[i];
+			}
+		}
+		//Print second biggest number in array
+		System.out.println("Second biggest number in array: " + secondMax + "\n");
+	}
+
+	public void exercise_02 () {
+		System.out.println("---------------------------------\n");
+		System.out.println("Exercise 02\n");
+
+		//My array
+		String [] expression = {"'stset lanoitcnuf evol i'"};
+
+		//Print my array
+		System.out.println("Text in array: " + Arrays.toString(expression));
+
+		//Convert string array to char array
+		String s = "";
+		for (String n: expression) {
+			s = n;
+			char[] c = s.toCharArray();
+
+			//Flip the text
+			System.out.print("Reversed array: ");
+			for (int i = c.length - 1; i >=0; i--) {
+				System.out.print(c[i]);
+			}
+		}
+
+		
+	}
+}
---
+++ Totango/src/com/totango/MainTotango.java
@@ -0,0 +1,15 @@
+package com.totango;
+
+public class MainTotango {
+
+	public static void main(String[] args) {
+		
+		AllExercise myExercise = new AllExercise();
+		
+		myExercise.exercise_01();
+		
+		myExercise.exercise_02();
+
+	}
+
+}
